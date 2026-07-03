030aaa6d6d0420e6ba7da57084023b876b9a4d26
riskinurhadi <rizkibinmangtrisno@gmail.com>
2026-07-03 16:54:22 +0700

    modified:   proses.php

--- proses.php
+++ proses.php
@@ -52,7 +52,7 @@ try {
     $status_masuk = bersihkan($conn, $_POST['status_masuk']);
     
     // Tentukan Prefix berdasarkan status masuk
-    $prefix = "SB"; // Default Santri Baru
+    $prefix = "Santri Baru"; // Default Santri Baru
     if ($status_masuk == "Pindahan") $prefix = "PD";
     if ($status_masuk == "Drop Out") $prefix = "DO";
 
