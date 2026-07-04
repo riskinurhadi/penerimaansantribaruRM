7740d85ac71f76d92606eb972318be4cea232897
riskinurhadi <rizkibinmangtrisno@gmail.com>
2026-07-04 11:55:37 +0700

    modified:   admin/cetak_kts.php

--- admin/cetak_kts.php
+++ admin/cetak_kts.php
@@ -154,7 +154,7 @@ printRow($pdf, $x_text, $y_start + ($jarak * 4), 'Alamat', $alamat, true);
 
 // 4. TANDA TANGAN (Posisi Kanan Bawah - Digeser maksimal ke sudut)
 $x_ttd = 62; // Geser lebih ke kanan (dari 55 ke 62)
-$y_ttd_start = 35; // Geser sedikit ke bawah (dari 40 ke 41)
+$y_ttd_start = 39; // Geser sedikit ke bawah (dari 40 ke 41)
 $w_ttd = 26;
 
 $pdf->SetTextColor(11, 74, 105);
@@ -166,7 +166,7 @@ $pdf->SetX($x_ttd);
 $pdf->Cell($w_ttd, 3, 'Pimpinan', 0, 1, 'C');
 
 // Ruang kosong untuk stempel/paraf
-$pdf->SetXY($x_ttd, 52); // Geser nama pimpinan lebih ke bawah
+$pdf->SetXY($x_ttd, 49); // Geser nama pimpinan lebih ke bawah
 $pdf->SetFont('Arial', 'B', 6);
 $pdf->Cell($w_ttd, 3, 'Ust. Oktawidodo, S.Pd.I.', 0, 1, 'C');
 
